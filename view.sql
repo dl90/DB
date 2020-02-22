f5beec47dff38ff71d8246327a454008cc01adcf
dl90 <48544754+dl90@users.noreply.github.com>
2020-02-22 13:45:06 -0800

    1

--- view.sql
+++ view.sql
@@ -1,16 +1,22 @@
 -- Database view (view) is used to store queries on to the db so it can be reused
 -- Note a vew and a tabel can not have the same name
+-- You can created views based on existing views
 
+-- CREATE [OR REPLACE] VIEW [db_name.]view_name [(column_list)]
 CREATE VIEW view_name AS 
-SELECT 
-  customerName, 
-  checkNumber, 
-  paymentDate, 
-  amount
-FROM
-  customers
-INNER JOIN
-  payments USING (customerNumber);
+  SELECT 
+    customerName, 
+    checkNumber, 
+    paymentDate, 
+    amount
+  FROM
+    customers
+  INNER JOIN
+    payments USING (customerNumber);
 
 -- To use the saved query
 SELECT * FROM view_name;
+
+-- To see if tables are table or view
+SHOW FULL TABLES;
+
